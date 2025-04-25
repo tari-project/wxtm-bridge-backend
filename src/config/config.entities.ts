716309eaed04904f47f751b8930c2f14dd06249d
Patryk Wargacki <wargacki.patryq@gmail.com>
2025-04-25 13:41:18 +0200

    subgraph added to config

--- src/config/config.entities.ts
+++ src/config/config.entities.ts
@@ -1,6 +1,6 @@
 import { UserEntity } from '../user/user.entity';
 import { WrapTokenTransactionEntity } from '../wrap-token-transaction/wrap-token-transaction.entity';
-import { TokensUnwrappedEntity } from '../subgraph/tokens-unwrapped.entity';
+import { TokensUnwrappedEntity } from '../tokens-unwrapped/tokens-unwrapped.entity';
 
 export const entities = [
   UserEntity,
