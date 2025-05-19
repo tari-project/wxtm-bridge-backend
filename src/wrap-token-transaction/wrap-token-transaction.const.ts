b9dde633a07a73e291a49e85a7225671d03aeec6
marius <asmarenas@gmail.com>
2025-05-19 15:09:33 +0200

    Resolve conflicts

--- src/wrap-token-transaction/wrap-token-transaction.const.ts
+++ src/wrap-token-transaction/wrap-token-transaction.const.ts
@@ -2,5 +2,7 @@ export enum WrapTokenTransactionStatus {
   CREATED = 'created',
   TOKENS_SENT = 'tokens_sent',
   TOKENS_RECEIVED = 'tokens_received',
+  CREATING_SAFE_TRANSACTION = 'creating_safe_transaction',
   SAFE_TRANSACTION_CREATED = 'safe_transaction_created',
+  UNPROCESSABLE = 'unprocessible',
 }
