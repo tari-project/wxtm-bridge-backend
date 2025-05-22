523b0531ce10a44fb77f7f66629a7a27f7c3a972
marius <asmarenas@gmail.com>
2025-05-22 22:35:48 +0200

    Lint code

--- src/migrations/1747945667659-UpdateWrapTokenEntity.ts
+++ src/migrations/1747945667659-UpdateWrapTokenEntity.ts
@@ -1,14 +1,17 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class UpdateWrapTokenEntity1747945667659 implements MigrationInterface {
-    name = 'UpdateWrapTokenEntity1747945667659'
+  name = 'UpdateWrapTokenEntity1747945667659';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "wrap_token_transactions" RENAME COLUMN "tariTxId" TO "tariPaymentIdHex"`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "wrap_token_transactions" RENAME COLUMN "tariTxId" TO "tariPaymentIdHex"`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "wrap_token_transactions" RENAME COLUMN "tariPaymentIdHex" TO "tariTxId"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "wrap_token_transactions" RENAME COLUMN "tariPaymentIdHex" TO "tariTxId"`,
+    );
+  }
 }
