dfa3dbe7883dd9c202bfeab640304f6a37ab77d4
marius <asmarenas@gmail.com>
2025-09-11 11:41:03 +0200

    Calculate wallet balances

--- src/payment-wallet-balance/payment-wallet-balance.service.ts
+++ src/payment-wallet-balance/payment-wallet-balance.service.ts
@@ -3,14 +3,21 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
 import { PaymentWalletBalanceEntity } from './payment-wallet-balance.entity';
-import { PaymentWalletBalanceDTO } from './payment-wallet-balance.dto';
+import {
+  PaymentWalletBalanceDTO,
+  PaymentWalletBalanceResponseDTO,
+} from './payment-wallet-balance.dto';
 import { SuccessDTO } from '../dto/success.dto';
+import { TokensUnwrappedEntity } from '../tokens-unwrapped/tokens-unwrapped.entity';
+import { TokensUnwrappedStatus } from '../tokens-unwrapped/tokens-unwrapped.const';
 
 @Injectable()
 export class PaymentWalletBalanceService {
   constructor(
     @InjectRepository(PaymentWalletBalanceEntity)
     private readonly paymentWalletBalanceRepository: Repository<PaymentWalletBalanceEntity>,
+    @InjectRepository(TokensUnwrappedEntity)
+    private readonly tokensUnwrappedRepository: Repository<TokensUnwrappedEntity>,
   ) {}
 
   async setBalance({
@@ -31,5 +38,46 @@ export class PaymentWalletBalanceService {
     });
 
     return { success: true };
+  }
+
+  private async getWalletBalance(): Promise<bigint> {
+    const { availableBalance, pendingIncomingBalance } =
+      await this.paymentWalletBalanceRepository.findOneByOrFail({
+        id: 1,
+      });
+
+    return BigInt(availableBalance) + BigInt(pendingIncomingBalance);
+  }
+
+  private async getPendingTransactionsAmount(): Promise<bigint> {
+    const transactionSumResult = await this.tokensUnwrappedRepository
+      .createQueryBuilder('transaction')
+      .select('SUM(transaction.amountAfterFee)', 'total')
+      .where('transaction.status NOT IN (:...excludedStatuses)', {
+        excludedStatuses: [
+          TokensUnwrappedStatus.INIT_SEND_TOKENS,
+          TokensUnwrappedStatus.SENDING_TOKENS,
+          TokensUnwrappedStatus.TOKENS_SENT,
+          TokensUnwrappedStatus.SENDING_TOKENS_UNPROCESSABLE,
+        ],
+      })
+      .getRawOne<{ total: string }>();
+
+    return transactionSumResult?.total
+      ? BigInt(transactionSumResult.total)
+      : BigInt(0);
+  }
+
+  async getBalances(): Promise<PaymentWalletBalanceResponseDTO> {
+    const walletBalance = await this.getWalletBalance();
+    const pendingTransactionsAmount = await this.getPendingTransactionsAmount();
+
+    const availableWalletBalance = walletBalance - pendingTransactionsAmount;
+
+    return {
+      walletBalance: walletBalance.toString(),
+      pendingTransactionsAmount: pendingTransactionsAmount.toString(),
+      availableWalletBalance: availableWalletBalance.toString(),
+    };
   }
 }
